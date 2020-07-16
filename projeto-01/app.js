5532ad1dd61efbffe11a297efd3667e40274a49d
Alisson Santana <alissonsantanapo@gmail.com>
2020-07-16 16:47:41 -0300

    update-projeto-1

--- projeto-01/app.js
+++ projeto-01/app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         porcentJogador: 100,
         porcentMonstro: 100,
-        rodando: false 
+        rodando: false,
+        logs: []
     },
     computed: {
         hasResult() {
@@ -19,23 +20,37 @@ new Vue({
         iniciarJogo(){
             this.rodando = true,
             this.porcentJogador = 100,
-            this.porcentMonstro = 100
+            this.porcentMonstro = 100,
+            this.logs= []
         },
         ataque(especial){
-            this.dano('porcentJogador', 7, 12, false)
-            this.dano('porcentMonstro', 5, 10, especial)
+            this.dano('porcentMonstro', 5, 10, especial, 'Jogador', 'Monstro', 'jogador')
+            if(this.porcentMonstro > 0) {
+                this.dano('porcentJogador', 7, 12, false, 'Monstro', 'Jogador', 'monstro')
+            }
+            
         },
-        dano(atr, min, max, especial) {
+        dano(atr, min, max, especial, origem, alvo, cls) {
             const plus = especial ? 5 : 0
             const dano = this.geraNumero(min + plus, max + plus)
             this[atr] = Math.max(this[atr] - dano, 0)
+            this.registraLog(`${origem} atingiu ${alvo} com ${dano}.`, cls)
         },
-        curar() {
-
+        curaEAtaca() {
+            this.curar(10, 15)
+            this.ataque('porcentJogador', 7, 12, false, 'Monstro', 'Jogador', 'monstro')
+        },
+        curar(min, max) {
+            const cura = this.geraNumero(min, max)
+            this.porcentJogador = Math.min(this.porcentJogador + cura, 100)
+            this.registraLog(`Jogador ganhou força de ${cura}.`, 'jogador')
         },
         geraNumero(min, max){
             const value = Math.random() * (max - min) + min
             return Math.round(value)
+        },
+        registraLog(text, cls) {
+            this.logs.unshift({text, cls})
         }
 
 
